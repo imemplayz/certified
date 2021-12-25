c9dde73b04b93dbdc2fb087ae63a2ebd3cc0f7e2
imemplayz <63793473+imemplayz@users.noreply.github.com>
2021-12-25 14:36:51 +0100

    e

--- script.js
+++ script.js
@@ -1,64 +1,31 @@
-function preview() {
-    //english
-    var firstNameInput = document.getElementById("firstNameInput").value;
-    var lastNameInput = document.getElementById("lastNameInput").value;
-    var birthDateInput = document.getElementById("birthDateInput").value;
-    var guardianInput = document.getElementById("guardianInput").value;
-    var effectiveDateInput = document.getElementById("effectiveDateInput").value;
-    var firstName = document.getElementById("firstName");
-    var lastName = document.getElementById("lastName");
-    var birthDate = document.getElementById("birthDate");
-    var guardian = document.getElementById("guardian");
-    var effectiveDate = document.getElementById("effectiveDate");
-    var passId = document.getElementById("passID");
-    var idNumber = getRandomInt(111111111, 999999999)
-    //arabic
-    var firstNameInputArabic = document.getElementById("firstNameInputArabic").value;
-    var lastNameInputArabic = document.getElementById("lastNameInputArabic").value;
-    var guardianInputArabic = document.getElementById("guardianInputArabic").value;
-    var firstNameArabic = document.getElementById("firstNameArabic");
-    var lastNameArabic = document.getElementById("lastNameArabic");
-    var nameArabic = document.getElementById("nameArabic");
-    var guardianArabic = document.getElementById("guardianArabic");
-    var effectiveDateArabic = document.getElementById("effectiveDateArabic");
-
-
-    firstName.textContent = firstNameInput + " "
-    lastName.textContent = lastNameInput
-    birthDate.textContent = birthDateInput
-    guardian.textContent = guardianInput
-    effectiveDate.textContent = effectiveDateInput
-    passId.textContent = idNumber
-    //arabic
-    nameArabic.textContent = firstNameInputArabic + " " + lastNameInputArabic
-    birthDateArabic.textContent = birthDateInput
-    guardianArabic.textContent = guardianInputArabic
-    effectiveDateArabic.textContent = effectiveDateInput
-}
-
-/////////////////////////////////////
-
-function Print()
-{
-    var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-    mywindow.document.write('<html><head><title></title>');
-    mywindow.document.write('<style>@page { size: auto;  margin: 0mm; } </style>');
-    mywindow.document.write('</head><body >');
-    mywindow.document.write(document.getElementById("printDiv").innerHTML);
-    mywindow.document.write('</body></html>');
-
-    mywindow.document.close(); // necessary for IE >= 10
-    mywindow.focus(); // necessary for IE >= 10*/
-
-    mywindow.print();
-    mywindow.close();
-
-    return true;
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+    scanner.addListener('scan',function(content){
+        alert(content);
+        //window.location.href=content;
+    });
+    Instascan.Camera.getCameras().then(function (cameras){
+        if(cameras.length>0){
+            scanner.start(cameras[0]);
+            $('[name="options"]').on('change',function(){
+                if($(this).val()==1){
+                    if(cameras[0]!=""){
+                        scanner.start(cameras[0]);
+                    }else{
+                        alert('No Front camera found!');
+                    }
+                }else if($(this).val()==2){
+                    if(cameras[1]!=""){
+                        scanner.start(cameras[1]);
+                    }else{
+                        alert('No Back camera found!');
+                    }
+                }
+            });
+        }else{
+            console.error('No cameras found.');
+            alert('No cameras found.');
+        }
+    }).catch(function(e){
+        console.error(e);
+        alert(e);
+    });
